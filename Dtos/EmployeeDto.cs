e7741f8202fdb1634764f4b939f8955f4759f6c3
KowsikaK <kowsika.k@winomech.com>
2026-05-07 11:36:07 +0530

    employee dto created

--- Dtos/EmployeeDto.cs
+++ Dtos/EmployeeDto.cs
@@ -2,5 +2,14 @@
 {
     public class EmployeeDto
     {
+        public Guid EmployeeId { get; set; }
+
+        public string? EmployeeName { get; set; }
+
+        public string? EmployeeEmail { get; set; }
+
+        public DateTime EmployeeDob { get; set; }
+
+        public decimal EmployeeSalary { get; set; }
     }
 }
